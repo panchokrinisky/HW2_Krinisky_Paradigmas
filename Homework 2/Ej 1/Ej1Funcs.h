d23459d5a09d4c50f0bb4f5b50cafb34be8505e8
panchokrinisky <161512362+panchokrinisky@users.noreply.github.com>
2025-03-31 02:01:39 -0300

    Add files via upload

---
+++ Homework 2/Ej 1/Ej1Funcs.h
@@ -0,0 +1,8 @@
+#include "Time.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdexcept>
+
+
+Time create_time(void);
---
+++ Homework 2/Ej 2/Ej2Funcs.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <memory>
+#include <algorithm>
+#include "Estudiante.h"
+#include "Curso.h"
+
+using namespace std;
+
+
+Curso crearCurso();
+
+
+void menuCurso(Curso curso);
+
